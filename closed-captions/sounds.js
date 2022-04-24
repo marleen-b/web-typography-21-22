78b16e1572537c29c3a1ffda5123137e6340a06c
marleen-b <70547716+marleen-b@users.noreply.github.com>
2022-04-24 21:14:18 +0200

    JS comments
    Voor gemak comments geplaatst wanneer welke sound komt. Dus vanaf 12 seconden is sound 2 beschikbaar

--- closed-captions/sounds.js
+++ closed-captions/sounds.js
@@ -3,13 +3,15 @@
 sounds = [
 	0, //sound 0
 	9, //sound 1
-	12,
-	15,
-	16.5,
-	24,
-	25.8,
-	31,
-	32,
-	34,
-	86
+	12, //sound 2
+	15, //sound 3
+	16.5, //sound 4
+	24, //sound 5
+	25.8, //sound 6
+	31, //sound 7
+	32, //sound 8
+	34, //sound 9
+	86 //sound 10
 ];
+
+// Als je een nieuwe sound toevoegd opletten dat de volgorde veranderd dus sound 1 wordt misschien sound 2, pas dit ook aan in de css
