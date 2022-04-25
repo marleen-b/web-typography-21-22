627aa74ab9c015d03c7610cf718a302639fdc44d
marleen-b <70547716+marleen-b@users.noreply.github.com>
2022-04-25 21:23:35 +0200

    Sound 4
    Toevoegen van code voor sound4; warning sound. Werkt alleen niet zoals ik het wil hebben

--- closed-captions/sounds.js
+++ closed-captions/sounds.js
@@ -2,18 +2,20 @@
 // Op die mommenten komt er een class op de body.
 sounds = [
 	0, //sound 0
-	9, //sound 1
-	12, //sound 2
-	15, //sound 3
-	16.5, //sound 4
-	24, //sound 5
-	25.8, //sound 6
-	31, //sound 7
+	9, //sound 1 = 0:09 alarm sound
+	12, //sound 2 = 0:12 alarm sound v2
+	15, //sound 3 = 0:15
+	16.5, //sound 4 = 0:16 warning sound
+	24, //sound 5 = 0.24 begin pieptoon
+	25.8, //sound 6 = 0:25 warning sound herhaling
+	31, //sound 7 = warning sound
 	32, //sound 8
-	34, //sound 9
+	34, //sound 9 = 0:34 pieptoon klinkt harder
 	75, //sound 10
-	86 //sound 11
+	86 //sound 11 = 1:26 soort van soundwaves
 ];
 
 // Als je een nieuwe sound toevoegd opletten dat de volgorde veranderd dus sound 1 wordt misschien sound 2, pas dit ook aan in de css
-//
+// pieptoon; soort van noise effect wat steeds erger wordt.
+// Als dan het geluid stopt en de knal van stilte komt wordt de BG wit ipv zwart
+// vanaf 35 seconden hoor je de piep echt goed.
